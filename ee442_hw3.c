41b70b82a16ee37625adaf2232315017f11ebe41
Y2P <yigit.pilavci01@gmail.com>
2017-05-14 17:04:38 +0300

    final commit

--- ee442_hw3.c
+++ ee442_hw3.c
@@ -7,187 +7,157 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#define EMPTY 0
+// Predefined Thread States
+#define FINISHED 0
 #define READY 1 
-#define FINISHED  2
-#define RUNNING 3
+#define RUNNING 2
 #define MAX_THREAD 5
-// TODO context array is changed to ThreadInfo array
-// 		Parallel threads will be created 
-// 		Signals and alarm will be resarched in detail
- 
 
 #define STACK_SIZE 4096
-int current_context = 0;
+
+// Global variables are  defined here
+int current_context = 0; // For current thread index	
 int context_num = 0;
-int prev_context= 0;
+int prev_context= 0;	// Holding previous thread index
+
+// Structure for thread information ( Context and state)
 struct ThreadInfo
 {
 	ucontext_t context;
 	int state; 
 };
 
-
-volatile sig_atomic_t print_flag = false;
+// Thread Array used for the round robin
 struct ThreadInfo ThreadArr[MAX_THREAD];
 
 // ISR for catching CTRL-C
 void  INThandler(int sig)
 {
 	int i ;
-	for(i = 0; i < MAX_THREAD ; i++)
+
+	printf("\nReleasing the allocated memory\n");
+	// Deleting the dynamically allocated memory
+	for(i = 1; i < MAX_THREAD ; i++)
 	{
 		free(ThreadArr[i].context.uc_stack.ss_sp);
 	}
+
+	// 	And exit to terminal
+	printf("Exiting... (Bye bye) \n");
 
 	exit(0);
 
 }
 
 
+// Method for checking FINISHED places in given Thread Array
+// Returns the first FINISHED place. If there is not, returns 0
 int HasEmptyPlace(struct ThreadInfo ThreadArr[MAX_THREAD])
 {
 	int i;
-	for (i = 0; i < MAX_THREAD ; i++)
+	for (i = 1; i < MAX_THREAD ; i++)
 	{
-
-		if(ThreadArr[i].state == EMPTY)
+		if(ThreadArr[i].state == FINISHED)
 			return i;
 	}
 	return 0;
 }
+// Worker function for the threads. Takes two parameters (n, tabnum)
+// n is for the number of count
+// tabnum is for number of printed tabs.
 void func(int n,int tabnum)
 {
-	int idx; 
-	idx = (tabnum)%MAX_THREAD;
-	if(idx == 0)
-		idx++;
-	//ThreadArr[idx].state = RUNNING;
-	//printf("n: %d, tabnum: %d\n",n,tabnum);
 	int x = 0;
 	int y = 0;
+	ThreadArr[current_context].state = RUNNING;
+
+	// Print operation is done here
 	for (x = 0 ; x < n ; x++){
 		for (y = 0 ; y < tabnum ; y++)
 			printf("\t");
 		printf("%d\n", x);
 		usleep(100000);
 	}
-
-	//printf("%d\n",idx );
-	//free(ThreadArr[current_context].context.uc_stack.ss_sp); 
+	// Free the place in the thread array for the waiting threads in the queue
 	ThreadArr[current_context].context.uc_stack.ss_size = 0;
-	printf("idx: %d --> EMPTY\n",current_context );
-	ThreadArr[current_context].state = EMPTY;
-	//current_context = 0;
-	raise(SIGALRM);
-	//alarm(1);
-	//usleep(100);
-	//printf("FINISHED: %d\n", n);
+	ThreadArr[current_context].state = FINISHED;
 
+	// Switch to the new thread or main
+	raise(SIGALRM);
 }
+
+// Interrupt handler function for the scheduler
+// This interrupt occurs in 1 second after its alarm is set
+// At the end of 1 second, this function operates and switch the context
+// to a thread in the thread array which Round Robin scheduling proposes
 void context_swapper(int sig)
 {
-	//printf("Signal hit\n");
-	print_flag = true;
-	int i ; 
-	//printf("%d\n", current_context);
-/*	for (i = 1; i < MAX_THREAD; i++)
-	{
-		//printf("%d\n",  ThreadArr[i].state);
-		if(i == MAX_THREAD-1 && ThreadArr[i].state == EMPTY)
-		{
-	//		printf("EMPTY\n");
-			alarm( 1 ); // before scheduling it to be called.
-			return;
-		}
-		else if(ThreadArr[i].state != EMPTY)
-			break;
-	}
-*/	
+	// For recording the previous context, take the index of it.
 	prev_context = current_context;
-//	printf("PREV_context: %d state: %d\n",current_context,ThreadArr[current_context].state);
 
+	// Update the current context
 	do
 	{
-		//printf("%d/n",(ThreadArr[current_context].context.uc_stack.ss_size));
 		current_context++;			
-		//printf("current_context: %d state: %d\n",current_context,ThreadArr[current_context].state);
-
 		current_context = current_context % MAX_THREAD;
-		//if(current_context == 0)
-		//	current_context++;
-	//	printf("Update current context: %d %d\n",current_context,prev_context);
-	//	printf("%d\n", ThreadArr[current_context].state );
-	}while(ThreadArr[current_context].state != READY);
-	printf("Previous Context = %d Current Context = %d\n",prev_context,current_context);
-	//while((ThreadArr[current_context].context.uc_stack.ss_size)==0);
-	//printf("Signal hit done\n");
-	//if(ThreadArr[current_context].context.uc_stack.ss_size)
+	}while(ThreadArr[current_context].state != READY && ThreadArr[current_context].state != RUNNING );
+
+	// Set the alarm to 1 second again
 	alarm( 1 ); // before scheduling it to be called.
 
+	// Save the previous context
 	getcontext(&ThreadArr[prev_context].context);
+	// Switch to new context
 	swapcontext(&ThreadArr[prev_context].context, &ThreadArr[current_context].context);
-
-
 }
-int idx;
-//int idx[4] = {1 , 2 , 3, 4};
-int i ;
 
 int main(int argc, char const *argv[])
 {
-  //  printf("CTRL-C to quit\n");
-	//signal(SIGINT, INThandler);
+	// To end program properly, CTRL-C is caught by signal interrupt
+    printf("CTRL-C to quit\n");
+	signal(SIGINT, INThandler);
 
-	signal( SIGALRM, context_swapper ); // Install handler first,
-	alarm( 1 ); // before scheduling it to be called.
+	int i ;
+	int idx;
+
+	// Context switching interrupt is defined here
+	signal( SIGALRM, context_swapper ); 
+	alarm( 1 ); 
 	ucontext_t c;
 
+	// Initialize the threads 
 	for (i = 0; i < MAX_THREAD ; i++)
 	{
-		ThreadArr[i].state = EMPTY;
+		ThreadArr[i].state = FINISHED;
 	}
 
-	context_num = argc;
+	// First thread is the main function
 	getcontext(&c);
 	ThreadArr[0].context = c;
 	ThreadArr[0].state = READY;
-	//ThreadArr = malloc(argc*sizeof(struct ThreadInfo));
+
+	// For each thread 
 	for (i = 1; i < argc ; i++)
 	{
-
+		// Wait until an empty space exists in thread array
 		while((idx = HasEmptyPlace(ThreadArr)) == 0)
-		{
-			printf("Main\n");
-			raise(SIGALRM);
-		}
+			raise(SIGALRM); // Eliminate the idle state
 		
-		//getcontext(&c);
-		
+		getcontext(&c);
+
+		// Initialize and create a new thread with its context
 		c.uc_link = &ThreadArr[0].context;
 		c.uc_stack.ss_sp = malloc(STACK_SIZE);
 		c.uc_stack.ss_size = STACK_SIZE;
 
 		makecontext(&c,(void (*)(void))func,2,atoi(argv[i]),i);
-		//printf("%d\n",current_context );
-		//printf("Main Loop\n");
-		//printf("idx:%d\n", idx[(i-1)%4]);
-		//printf("idx: %d\n",idx );
-		printf("idx: %d --> READY\n",idx );
-
+	
+		// Place it to the next empty place and turn its space to READY
 		ThreadArr[idx].context = c;
 		ThreadArr[idx].state = READY;
-
-
-		//	printf("%d : %d\n",i%MAX_THREAD,ThreadArr[i%MAX_THREAD].state);
-		//
-		//	printf("%d\n",i );
-	//	free(c.uc_stack.ss_sp);
 	}
-
-	while(1);
-
-	//free(ThreadArr);
-
+	while(1)
+		raise(SIGALRM); // Eliminate the idle state
+	
 	return 0;
 }
